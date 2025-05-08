912748e9609dde44c56e870f07c90b554f056a0e
Meng <hkongmeng50@gmail.com>
2025-05-08 17:11:12 +0700

    a

---
+++ wp-content_backup/themes/hello-elementor/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'elementor/hello-theme',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '19e947bbb78ac867c7d9d5b4a115384608938689',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'elementor/hello-theme' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '19e947bbb78ac867c7d9d5b4a115384608938689',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'elementor/wp-notifications-package' => array(
+            'pretty_version' => '1.2.0',
+            'version' => '1.2.0.0',
+            'reference' => 'dd25ca9dd79402c3bb51fab112aa079702eb165e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../elementor/wp-notifications-package',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
